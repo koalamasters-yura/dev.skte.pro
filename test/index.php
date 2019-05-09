38a4e475493d96dcb704315fa84034ab2cb04290
yura_koala <yura.l@koalamasters.com>
2019-05-09 15:15:03 +0300

    GIT test

---
+++ test/index.php
@@ -0,0 +1,10 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Test GIT");
+?>
+
+<div class="test">
+    GIT test
+</div>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
